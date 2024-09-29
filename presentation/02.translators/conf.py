fcd3a925f3b1379cfefc6fb492f4320f8ac75458
Mikhail Zelenyi <mihail.zelenyy@phystech.edu>
2024-09-29 12:34:58 +0300

    Add tranlslators presentation

---
+++ presentation/02.translators/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '02'
+copyright = '2023, SCP'
+author = 'SCP'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    "sphinx_revealjs",
+    "sphinxcontrib.plantuml"
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'ru'
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ presentation/02.translators/index.md
@@ -0,0 +1,12 @@
+# Реализации транслятора Python
+
+```{include} ../../source/cards/translators/translators.md
+:heading-offset: 1
+:relative-images:
+```
+
+```{include} ../../source/cards/translators/CPython_Jython_Bytecode.md
+:heading-offset: 1
+:relative-images:
+```
+
